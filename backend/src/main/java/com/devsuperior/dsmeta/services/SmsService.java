6ce62dea41b1e314009cbf13e9ca0e35757d5fe3
Alex José Velasco Nunes <alexjvnunes@gmail.com>
2022-09-09 16:09:23 -0300

    Twilio SMS

--- backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
@@ -1,8 +1,10 @@
 package com.devsuperior.dsmeta.services;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.devsuperior.dsmeta.entities.Sale;
+import com.devsuperior.dsmeta.repositories.SaleRepository;
 import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
@@ -10,26 +12,29 @@ import com.twilio.type.PhoneNumber;
 @Service
 public class SmsService {
 
-	@Value("${twilio.sid}")
-	private String twilioSid;
-
-	@Value("${twilio.key}")
-	private String twilioKey;
-
-	@Value("${twilio.phone.from}")
-	private String twilioPhoneFrom;
-
-	@Value("${twilio.phone.to}")
-	private String twilioPhoneTo;
- 
-	public void sendSms() {
+    public static final String twilioSid = System.getenv("TWILIO_SID");
+    public static final String twilioKey = System.getenv("TWILIO_KEY");
+    public static final String twilioPhoneFrom = System.getenv("TWILIO_PHONE_FROM");
+    public static final String twilioPhoneTo = System.getenv("TWILIO_PHONE_TO");
+    
+    @Autowired
+    private SaleRepository saleRepository;
+    
+	public void sendSms(Long saleId) {
+		
+		Sale sale = saleRepository.findById(saleId).get();
+		
+		String date = sale.getDate().getMonthValue() + "/" + sale.getDate().getYear();
+		
+		String msg = "Vendedor " + sale.getSellerName() + " foi destaque em " + date 
+				+ " com um total de R$ " + String.format("%.2f", sale.getAmount());
 
 		Twilio.init(twilioSid, twilioKey);
 
 		PhoneNumber to = new PhoneNumber(twilioPhoneTo);
 		PhoneNumber from = new PhoneNumber(twilioPhoneFrom);
 
-		Message message = Message.creator(to, from, "Teste").create();
+		Message message = Message.creator(to, from, msg).create();
 
 		System.out.println(message.getSid());
 	}
